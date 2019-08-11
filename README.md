77114f26135584581a12c51495a419824636e9ec
Abinash Shaw <nashcheez@gmail.com>
2019-08-12 01:27:28 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,26 +1,144 @@
-# pleaseChat
-A realtime chat application, similar to WhatsApp Web featuring a simple two column layout which has a list of friends on the left column, and chat history and compose box on the right.
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
 
-**Attaching screenshot:**
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/nashcheez/please-chat">
+    <img src="images/chat-icon.png" alt="chat-logo" width="80" height="80" />
+  </a>
 
-![pleaseChat screenshot](https://cloud.githubusercontent.com/assets/2913308/20969022/27b73d00-bcad-11e6-97a4-f0e9337b1cc4.png)
+  <h3 align="center">Please Chat</h3>
 
+  <p align="center">
+    A chat application built on WebSocket
+    <br />
+    <br />
+    <a href="https://nashcheez.github.io/please-chat/" target="_blank">View Demo</a>
+    ·
+    <a href="https://github.com/nashcheez/please-chat/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/nashcheez/please-chat/issues">Request Feature</a>
+  </p>
+</p>
 
-**Few features/implementations in the code:**
+<!-- TABLE OF CONTENTS -->
 
-* The pleaseChat application lets you search for a friend and start a chat with him/her. You can send messages to this friend and see the echoed replies in a typical messaging format. (The friends list is currently harcoded)
+## Table of Contents
 
-* Switching between friends lets you instantly see the appropriate chat history.
+-   [About the Project](#about-the-project)
+    -   [Key Features](#key-features)
+    -   [Built With](#built-with)
+-   [Getting Started](#getting-started)
+    -   [Installation](#installation)
+-   [Roadmap](#roadmap)
+-   [Contributing](#contributing)
+-   [License](#license)
+-   [Contact](#contact)
+-   [Acknowledgements](#acknowledgements)
 
-* The entire application is built using HTML, SCSS, JS & jQuery. It has been built on a Mac (macOS Sierra) using Sublime Text 3 and tested on Chrome, Firefox & Safari. The ST plugin ‘Sass Build’ has been used to convert the scss into css in minified form.
+<!-- ABOUT THE PROJECT -->
 
-*  I have tried to maintain a modular and OOP architecture while implementing encapsulation, where all the variables, functions & events are attributed to a single object in the script.js file.
+## About The Project
 
-* I have used an existing WebSocket echo service (https://www.websocket.org/echo.html) to fetch the chats here. Apart from building the regular features of the Web socket connection, the friend list search, transfer and echo of messages, I have also implemented error handling where the appropriate error messages are displayed on the closure of a connection etc. There is also a toggle switch provided to toggle between the connection status (online/offline).
+[![Product Name Screen Shot][product-screenshot]](https://nashcheez.github.io/please-chat/)
 
-* The UI is inspired from the iMessage application on the mac. The theme of the chat has been a comical conversation between superheroes. The conversations have been picked up from the Twitter handle (@FromSuperheroes) & IMDb.
+Please Chat is an application similar to WhatsApp Web/iMessage featuring a two column layout which has a list of friends and compose box on the left column, and chat history on the right.
 
-* I have used the best practices as much as possible for JavaScript & SCSS. I have tried to minimize DOM access wherever I could. And the selective nesting of the css classes has been done appropriately.
+### Key Features
 
-* I have provided comments wherever possible for a better understanding of the code.
+-   The application lets you search for a friend and start a chat with him. The user interface is inspired from the iMessage application on the Mac OS.
 
+-   One can send a message and see the replies echoed in a typical message format. Switching between friends lets you instantly see the corresponding chat history.
+
+-   Error handling has been implemented where apt error messages are displayed (connection closure, network failure etc).
+
+-   There is a toggle to switch between the connection status (online/offline).
+
+-   The app has an object oriented architecture implementing encapsulation and all the global variables & functions are attributed to a single `pleaseChat` object.
+
+### Built With
+
+The application has been developed using HTML, SCSS, JavaScript and jQuery.
+
+A [WebSocket echo service](https://www.websocket.org/echo.html) has been used to fetch the chats & acts as the connection layer.
+
+For more information about jQuery, please visit the official docs.
+
+-   [JQuery](https://jquery.com)
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To run the application, just open `index.html` in your browser.
+
+### Installation
+
+1. Clone the repo
+
+```sh
+git clone https://github.com/nashcheez/please-chat.git
+```
+
+2. Open `index.html`
+
+<!-- ROADMAP -->
+
+## Roadmap
+
+See the [open issues](https://github.com/nashcheez/please-chat/issues) for a list of proposed features (and known issues).
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the project
+2. Create your feature branch (`git checkout -b feature/amazing-feature`)
+3. Commit your changes (`git commit -m 'add some amazing feature'`)
+4. Push to the branch (`git push origin feature/amazing-feature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- CONTACT -->
+
+## Contact
+
+[@nashcheez](https://twitter.com/nashcheez) - nashcheez@gmail.com
+
+Project Link: [https://github.com/nashcheez/please-chat](https://github.com/nashcheez/please-chat)
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## Acknowledgements
+
+-   [iMessage](https://developer.apple.com/imessage/)
+-   [FromSuperheroes](https://twitter.com/FromSuperheroes)
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/nashcheez/please-chat.svg?style=flat-square
+[contributors-url]: https://github.com/nashcheez/please-chat/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/nashcheez/please-chat.svg?style=flat-square
+[forks-url]: https://github.com/nashcheez/please-chat/network/members
+[stars-shield]: https://img.shields.io/github/stars/nashcheez/please-chat.svg?style=flat-square
+[stars-url]: https://github.com/nashcheez/please-chat/stargazers
+[issues-shield]: https://img.shields.io/github/issues/nashcheez/please-chat.svg?style=flat-square
+[issues-url]: https://github.com/nashcheez/please-chat/issues
+[license-shield]: https://img.shields.io/github/license/nashcheez/please-chat.svg?style=flat-square
+[license-url]: https://github.com/nashcheez/please-chat/blob/master/LICENSE
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-blue.svg?style=flat-square&logo=linkedin&colorB=0077b5
+[linkedin-url]: https://linkedin.com/in/nashcheez
+[product-screenshot]: https://user-images.githubusercontent.com/2913308/62838677-411fc800-bc9d-11e9-8f4e-9f280541e2da.png
--- js/please-chat.js
+++ js/please-chat.js
@@ -23,30 +23,30 @@
             filterInput: $("#filter-input"),
             statusToggleSwitch: $(".bottom"),
             msgInput: $("#msg-input"),
-            sendBtn: $(".send")
+            sendBtn: $(".send"),
         },
         scrollToBottom: function() {
-            var activeChatWindow = $('.active-chat');
+            var activeChatWindow = $(".active-chat");
             activeChatWindow.scrollTop(activeChatWindow[0].scrollHeight + 70);
         },
         animatescrollToBottom: function() {
-            var activeChatWindow = $('.active-chat');
+            var activeChatWindow = $(".active-chat");
             activeChatWindow.animate({ scrollTop: activeChatWindow.prop("scrollHeight") }, 1000);
         },
         toggleChat: function() {
             var that = this;
-            $('.left .person').click(function() {
-                if ($(this).hasClass('.active')) {
+            $(".left .person").click(function() {
+                if ($(this).hasClass(".active")) {
                     return false;
                 } else {
                     var thisObj = $(this);
-                    var findChat = thisObj.attr('data-chat'),
-                        personName = thisObj.find('.name').text();
-                    $('.right .top .name').html(personName);
-                    $('.chat').removeClass('active-chat');
-                    $('.left .person').removeClass('active');
-                    thisObj.addClass('active');
-                    $('.chat[data-chat = ' + findChat + ']').addClass('active-chat');
+                    var findChat = thisObj.attr("data-chat"),
+                        personName = thisObj.find(".name").text();
+                    $(".right .top .name").html(personName);
+                    $(".chat").removeClass("active-chat");
+                    $(".left .person").removeClass("active");
+                    thisObj.addClass("active");
+                    $(".chat[data-chat = " + findChat + "]").addClass("active-chat");
                     that.scrollToBottom();
                 }
             });
@@ -54,13 +54,20 @@
         filterPeople: function() {
             var that = this;
             this.elements.chatHeadNames.each(function() {
-                $(this).attr('data-search-term', $(this).text().toLowerCase());
+                $(this).attr(
+                    "data-search-term",
+                    $(this)
+                        .text()
+                        .toLowerCase()
+                );
             });
-            this.elements.filterInput.on('keyup', function() {
-                var searchTerm = $(this).val().toLowerCase();
+            this.elements.filterInput.on("keyup", function() {
+                var searchTerm = $(this)
+                    .val()
+                    .toLowerCase();
                 that.elements.chatHeadNames.each(function() {
                     var that = $(this);
-                    if (that.filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+                    if (that.filter("[data-search-term *= " + searchTerm + "]").length > 0 || searchTerm.length < 1) {
                         that.parent().show();
                     } else {
                         that.parent().hide();
@@ -78,14 +85,27 @@
             var that = this;
             this.elements.statusToggleSwitch.click(function(event) {
                 var thisObj = $(this);
-                if (thisObj.children().first().hasClass("offline")) {
+                if (
+                    thisObj
+                        .children()
+                        .first()
+                        .hasClass("offline")
+                ) {
                     that.websocket.close();
-                    thisObj.children().first().removeClass("offline").addClass('online');
+                    thisObj
+                        .children()
+                        .first()
+                        .removeClass("offline")
+                        .addClass("online");
                     thisObj.children(".go-status").html("Go Online");
                 } else {
                     that.websocket = new WebSocket("ws://echo.websocket.org/");
                     that.webSocketHandlers();
-                    thisObj.children().first().removeClass("online").addClass('offline');
+                    thisObj
+                        .children()
+                        .first()
+                        .removeClass("online")
+                        .addClass("offline");
                     thisObj.children(".go-status").html("Go Offline");
                 }
             });
@@ -93,12 +113,16 @@
         sendMsg: function() {
             var that = this;
             this.elements.sendBtn.click(function() {
-                if (that.elements.msgInput.val() !== '') {
+                if (that.elements.msgInput.val() !== "") {
                     if (that.websocket.readyState == 1) {
                         that.websocket.send(that.elements.msgInput.val());
                         $(".active-chat").append("<div class='bubble me'>" + that.elements.msgInput.val() + "</div>");
                     } else {
-                        $(".active-chat").append("<div class='bubble me error'>" + that.elements.msgInput.val() + "</div><div class='error-msg'>Message could not be sent due to network error</div>");
+                        $(".active-chat").append(
+                            "<div class='bubble me error'>" +
+                                that.elements.msgInput.val() +
+                                "</div><div class='error-msg'>Message could not be sent due to network error</div>"
+                        );
                     }
                     that.elements.msgInput.val("");
                     that.animatescrollToBottom();
@@ -108,9 +132,9 @@
         sendMsgEnterHandler: function() {
             var that = this;
             this.elements.msgInput.keydown(function(event) {
-                var keyCode = (event.keyCode ? event.keyCode : event.which);
-                if (keyCode == 13 && that.elements.msgInput.val() !== '') {
-                    that.elements.sendBtn.trigger('click');
+                var keyCode = event.keyCode ? event.keyCode : event.which;
+                if (keyCode == 13 && that.elements.msgInput.val() !== "") {
+                    that.elements.sendBtn.trigger("click");
                 }
             });
         },
@@ -118,11 +142,11 @@
             var that = this;
             this.websocket.onopen = function(evt) {
                 that.elements.statusText.html("Online");
-                that.elements.indicator.removeClass('offline').addClass('online');
+                that.elements.indicator.removeClass("offline").addClass("online");
             };
             this.websocket.onclose = function(evt) {
                 that.elements.statusText.html("Offline");
-                that.elements.indicator.removeClass('online').addClass('offline');
+                that.elements.indicator.removeClass("online").addClass("offline");
             };
             this.websocket.onmessage = function(evt) {
                 $(".active-chat").append("<div class='bubble you'>" + evt.data + "</div>");
@@ -132,9 +156,8 @@
                 $(".active-chat").append("<div class='error-msg'>Network connection error</div>");
                 that.animatescrollToBottom();
             };
-        }
+        },
     };
 
     pleaseChat.init();
-
 })();
